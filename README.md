35af41516fdd73a0f3743f079edf04b0c149b27e
Lucas Nascimento Távora <38147859+tavoralucas@users.noreply.github.com>
2023-03-02 16:52:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# redes
+cadeira de redes
---
+++ server_tcp.py
@@ -0,0 +1,35 @@
+import socket
+ 
+HOST = '127.0.0.1'      # Endereco IP do Servidor
+PORT = 9090             # Porta que o Servidor está
+ 
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+origem = (HOST, PORT)
+ 
+# Colocando um endereço IP e uma porta no Socket
+tcp.bind(origem)
+ 
+# Colocando o Socket em modo passivo
+tcp.listen(1)
+ 
+print('\nServidor TCP iniciado no IP', HOST, 'na porta', PORT)
+ 
+while True:
+    # Aceitando uma nova conexão
+    conexao, cliente = tcp.accept()
+    print('\nConexão realizada por:', cliente)
+ 
+    while True:
+        # Recebendo as mensagens através da conexão
+        mensagem = conexao.recv(1024)
+        if not mensagem:
+            break
+ 
+        # Exibindo a mensagem recebida
+        print('\nCliente..:', cliente)
+        print('Mensagem.:', mensagem.decode())
+ 
+    print('Finalizando conexão do cliente', cliente)
+ 
+    # Fechando a conexão com o Socket
+    conexao.close()
